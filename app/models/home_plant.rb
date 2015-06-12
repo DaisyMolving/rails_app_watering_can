612320957ad0fdaa11d9680ab348d23ee4bb1e2b
Daisy Molving <daisymolving@gmail.com>
2015-06-12 13:00:04 +0100

    styled page and added illustrations

--- app/models/home_plant.rb
+++ app/models/home_plant.rb
@@ -7,8 +7,14 @@ class HomePlant < ActiveRecord::Base
   def get_precipitation
     url = "https://api.forecast.io/forecast/#{Rails.application.secrets.forecast_key}/51.5072,0.1275"
     @result = HTTParty.get(url)
-    binding.pry
-    nil
+
+    precipitation_intensity = @result['daily']['data'][0]['precipIntensity']
+
+
+    if (precipitation_intensity > 0.1) then 
+      home_plant.plant.amount_water_per_week = 0
+    
+    end
   end
 
 end
